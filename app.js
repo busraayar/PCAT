9a7d054d6326c2f472c29f351f173cdd43cf1fd9
busraayar <busrayar127@gmail.com>
2023-08-15 10:55:53 +0300

    committed

--- app.js
+++ app.js
@@ -3,27 +3,46 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./models/Photo');
+const { log } = require('console');
 
 const app = express();
 
-
 //connect DB
-mongoose.connect('mongodb://localhost/pcat-test-db');
-
+mongoose
+  .connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB CONNECTED!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 //TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
-
 //MIDDLEWARE
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
+
+app.get('/photos/:id', async (req, res) => {
+  console.log(req.params.id);
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -34,9 +53,9 @@ app.get('/photo', (req, res) => {
   res.render('photo');
 });
 
-app.post('/photos', async(req, res) => {
+app.post('/photos', async (req, res) => {
   await Photo.create(req.body);
-  res.redirect('/')
+  res.redirect('/');
 });
 
 let port = 2000;
